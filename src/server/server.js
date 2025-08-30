6932de9bed542aadf2ea0f1b8543de5a736af1f7
RajChoudhary007007 <raaz02256@gmail.com>
2025-08-30 19:36:28 +0530

    update url

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 // server.js
-// const SERVER_API_URL = 'https://backend.matteo-bianchi.com'; 
+const SERVER_API_URL = 'https://backend.matteo-bianchi.com'; 
 const SERVER_URL = 'http://localhost:3000'; 
-const SERVER_API_URL = 'http://localhost:8000'; 
+// const SERVER_API_URL = 'http://localhost:8000'; 
 
 export { SERVER_API_URL, SERVER_URL };
